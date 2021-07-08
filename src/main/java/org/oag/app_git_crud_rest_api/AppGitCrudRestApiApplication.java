de3dd3f7e87fb3c8fcceb5003091399f84bdd2bd
otajon <otajonazamov20@gmail.com>
2021-07-08 18:37:26 +0500

    birinchi commit

---
+++ src/main/java/org/oag/app_git_crud_rest_api/AppGitCrudRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.oag.app_git_crud_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGitCrudRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGitCrudRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oag/app_git_crud_rest_api/AppGitCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oag.app_git_crud_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGitCrudRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
